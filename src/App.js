0f1a581473449510b9ca26e6a898ef98534d22a0
Pavankumar Pendhota <pavankumar.pendhota@sparity.com>
2022-09-04 12:40:28 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from "react";
+import "./App.css";
+import Axios from "axios";
 
-function App() {
+
+
+function App(){
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+  const [movieReviewList, setMovieList] = useState([])
+  const [newReview, setNewReview] = useState("");
+  
+
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/api/get").then((response)=>{
+    setMovieList(response.data)
+    });
+  },[]);
+
+
+  // const [movieName, setMovieName] = useState("");
+  // const [review, setReview] = useState("");
+  // const [movieReviewList, setMovieList] = useState([])
+  const submitReview =() =>{
+    Axios.post("http://localhost:3001/api/insert", {
+      movieName: movieName,
+      movieReview: review,
+    });
+    setMovieList([
+      ...movieReviewList,
+      { movieName: movieName, movieReview: review },
+    ]);
+  };
+
+  // const submitHandler = e =>{
+  //   alert('Succussfully Submitted Data');
+  // }
+
+  const deleteReview=(movie)=>{
+    Axios.delete(`http://localhost:3001/api/delete/${movie}`);
+    alert("Deleted Successfully");
+  };
+
+  const updateReview=(movie)=>{
+    Axios.put("http://localhost:3001/api/update",{
+      movieName: movie,
+      movieReview: newReview,
+      
+    });
+    setNewReview("");
+    alert("Successfully Updated"); 
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <h1>Movie Review Form</h1>
+      <div className="form">
+        <label>Movie Name:</label>
+        <input type="text" name="movieName" onChange={(e)=>{
+          setMovieName(e.target.value)
+        }}/>
+        <label>Review:</label>
+        <input type="text" name="review"
+         onChange={(e)=>{
+        setReview(e.target.value)
+        }}/>
+
+        <button onClick={submitReview}>Submit</button>
+
+
+
+        {/* <label>Empcode</label> &nbsp;
+        <input type="text" /><br/>
+        <label>Empname</label> &nbsp;
+        <input type="text" /><br/>
+        <label>Salary</label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="text" /><br/>
+        <input type="button"onClick={submitHandler} value="Submit"/>&nbsp;
+        <input type="button" value="Insert"/>&nbsp;
+        <input type="button" value="Update"/>&nbsp;
+        <input type="button" value="Delete"/>&nbsp;
+        <input type="reset" Value="Clear" /> */}
+        
+
+        {movieReviewList.map((val)=>{
+          // return <h1>MovieName: {val.movieName} | Movie Review: {val.movieReview}</h1>
+                return( 
+                  <div className="card">
+                  <h1>{val.movieName} </h1> 
+                  <p> {val.movieReview} </p>
+
+                    <button onClick={()=>{deleteReview(val.movieName)}}>Delete</button>
+
+                    <input type="text" id="updateInput" onChange={(e)=>{
+                      setNewReview(e.target.value)
+                    }} />
+                    <button onClick={()=>{updateReview(val.movieName)}}>Update</button>
+
+                  </div>
+                );
+              })}
+        
+        </div>  
+      
+      
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+        <center>
+            <h1>Sign Up form</h1>
+            
+        </center>
+    </div>
+  )
+}
+
+export default login
